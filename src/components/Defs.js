3b64b07a59b4f0d098067ca3313dc81e40d86b66
Michael de Hoog <michael.dehoog@gmail.com>
2016-11-04 23:15:01 +1000

    Fixed image references

--- src/components/Defs.js
+++ src/components/Defs.js
@@ -6,22 +6,22 @@ export default class Defs extends Component {
         return (
             <defs>
                 <pattern id="btc" height="100%" width="100%" viewBox="0 0 1 1">
-                    <image width="1" height="1" xlinkHref="/images/btc.svg"/>
+                    <image width="1" height="1" xlinkHref={process.env.PUBLIC_URL + '/images/btc.svg'}/>
                 </pattern>
                 <pattern id="eth" height="100%" width="100%" viewBox="0 0 1 1">
-                    <image width="1" height="1" xlinkHref="/images/eth.svg"/>
+                    <image width="1" height="1" xlinkHref={process.env.PUBLIC_URL + '/images/eth.svg'}/>
                 </pattern>
                 <pattern id="ltc" height="100%" width="100%" viewBox="0 0 1 1">
-                    <image width="1" height="1" xlinkHref="/images/ltc.svg"/>
+                    <image width="1" height="1" xlinkHref={process.env.PUBLIC_URL + '/images/ltc.svg'}/>
                 </pattern>
                 <pattern id="usd" height="100%" width="100%" viewBox="0 0 1 1">
-                    <image width="1" height="1" xlinkHref="/images/usd.svg"/>
+                    <image width="1" height="1" xlinkHref={process.env.PUBLIC_URL + '/images/usd.svg'}/>
                 </pattern>
                 <pattern id="gbp" height="100%" width="100%" viewBox="0 0 1 1">
-                    <image width="1" height="1" xlinkHref="/images/gbp.svg"/>
+                    <image width="1" height="1" xlinkHref={process.env.PUBLIC_URL + '/images/gbp.svg'}/>
                 </pattern>
                 <pattern id="eur" height="100%" width="100%" viewBox="0 0 1 1">
-                    <image width="1" height="1" xlinkHref="/images/eur.svg"/>
+                    <image width="1" height="1" xlinkHref={process.env.PUBLIC_URL + '/images/eur.svg'}/>
                 </pattern>
             </defs>
         );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,15 +8,18 @@ export default class Footer extends Component {
                  style={{position: 'absolute', width: '100%', bottom: '10px', textAlign: 'center', fontSize: '12px'}}>
                 <span>Live data from </span>
                 <a href="https://coinbase.com/" target="_blank">
-                    <img src="/images/coinbase.svg" height="15" role="presentation"/>
+                    <img src={process.env.PUBLIC_URL + '/images/coinbase.svg'}
+                         height="15" role="presentation"/>
                 </a>
                 <span>. Made with </span>
                 <a href="https://facebook.github.io/react/" target="_blank">
-                    <img src="/images/react.svg" height="25" style={{marginBottom: '-7px'}} role="presentation"/>
+                    <img src={process.env.PUBLIC_URL + '/images/react.svg'}
+                         height="25" style={{marginBottom: '-7px'}} role="presentation"/>
                 </a>
                 <span>. Physics by </span>
                 <a href="http://brm.io/matter-js/" target="_blank">
-                    <img src="/images/matter-js.svg" height="18" style={{marginBottom: '-4px'}} role="presentation"/>
+                    <img src={process.env.PUBLIC_URL + '/images/matter-js.svg'}
+                         height="18" style={{marginBottom: '-4px'}} role="presentation"/>
                 </a>
                 <span>.</span>
             </div>
